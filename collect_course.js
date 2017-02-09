e2036e520fa6b47c763abab57a95c389477761e6
Daniel Wang <s92025592025@gmail.com>
2017-02-09 08:50:03 -0800

    Find way to parse HTML and manipulate with data

--- collect_course.js
+++ collect_course.js
@@ -2,18 +2,18 @@
 /*http://stackoverflow.com/questions/7372972/how-do-i-parse-a-html-page-with-node-js*/
 const fs = require('file-system');
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const cheerio = require('cheerio');
 
 
 var catalog = '';
 var request = new XMLHttpRequest();
-var parser = new DomParser();
 request.open('GET', 'https://www.washington.edu/students/timeschd/SPR2017/', false);
 request.onload = function (){
 	if(this.status == 200 || this.status == 0){
-		document = parser.parseFromString(this.responseText, 'text/html');
+		catalog = cheerio.load(this.responseText);
 	}
 }
 
 request.send();
 
-console.log(document);
+console.log(catalog('a').eq(2).attr('href'));

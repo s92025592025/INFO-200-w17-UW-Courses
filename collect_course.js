318a60832422136723007e056ff2ec9d16ca8021
Daniel Wang <s92025592025@gmail.com>
2017-02-09 14:05:15 -0800

    Deceided to parse by only html

--- collect_course.js
+++ collect_course.js
@@ -1,13 +1,13 @@
 'use strict';
-/*http://stackoverflow.com/questions/7372972/how-do-i-parse-a-html-page-with-node-js*/
+
 const fs = require('file-system');
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 const cheerio = require('cheerio');
-
+const BASE = 'https://www.washington.edu/students/timeschd/SPR2017/';
 
 var catalog = '';
 var request = new XMLHttpRequest();
-request.open('GET', 'https://www.washington.edu/students/timeschd/SPR2017/', false);
+request.open('GET', BASE, false);
 request.onload = function (){
 	if(this.status == 200 || this.status == 0){
 		catalog = cheerio.load(this.responseText);
@@ -16,4 +16,29 @@ request.onload = function (){
 
 request.send();
 
-console.log(catalog('a').eq(2).attr('href'));
+var major = '';
+console.log(catalog('a').eq(70).attr('href'));
+
+request.open('GET', BASE + '/' + catalog('a').eq(70).attr('href'), false);
+request.onload = function (){
+	if(this.status == 200 || this.status == 0){
+		major =  cheerio.load(this.responseText);
+	}
+}
+
+request.send()
+
+
+var course = '';
+for(var i = 0; i < major('a').length; i++){
+	if(/[0-9]{5}/.test(major('a').eq(i).text().trim())){
+		console.log(major('a').eq(i).attr('href'));
+		request.open('GET', major('a').eq(i).attr('href'), false);
+		request.onload = function (){
+			if(this.status == 200 || this.status == 0){}
+				course = cheerio.load(this.responseText);
+		};
+
+		request.send();
+	}
+}

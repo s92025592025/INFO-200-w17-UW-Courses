11fd80b7105a0ca3d28f1798b87c34f96c39a702
Daniel Wang <s92025592025@gmail.com>
2017-02-08 23:27:56 -0800

    See comment for HTML parsing suggestion

--- collect_course.js
+++ collect_course.js
@@ -1,3 +1,26 @@
-const jQuery = require('jQuery');
+'use strict';
+/*http://stackoverflow.com/questions/7372972/how-do-i-parse-a-html-page-with-node-js*/
 const fs = require('file-system');
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+require("jsdom").env("", function(err, window) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+ 
+    global.$ = require("jquery")(window);
+});
+
+var catalog = '';
+var request = new XMLHttpRequest();
+request.open('GET', 'https://www.washington.edu/students/timeschd/SPR2017/', false);
+request.onload = function (){
+	if(this.status == 200 || this.status == 0){
+		catalog = $.parseHTML();
+	}
+}
+
+request.send();
+
+console.log(catalog);

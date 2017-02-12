304cd1299421aeae525d85151e2bc0ecb0f33d20
Daniel Wang <s92025592025@gmail.com>
2017-02-11 23:46:50 -0800

    can get a list of major offering class next quarter

--- collect_course.js
+++ collect_course.js
@@ -7,16 +7,56 @@ const BASE = 'https://www.washington.edu/students/timeschd/SPR2017/';
 
 var catalog = webpageRequest(BASE);
 var major_list = [];
+var course_list = {};
+/*
+	course_list format:
+	{
+		major_abbr: [
+			{
+				num: ,
+				title: ,
+				sections: [
+					{
+						SLN: ,
+						section: ,
+						type: ,
+						credit: ,
+						meeting: [
+							{
+								day: [],
+								time: ,
+								building: ,
+								room:
+							}
+						],
+						instructor: "",
+
+					}
+				]
+
+			}
+		]
+	}
+*/
 
 // get all the link to classes information
 for(var i = 0; i < catalog('a').length; i++){
 	if(/\.html$/.test(catalog('a').eq(i).attr('href')) &&
 		/\([A-Z]+\)/.test(catalog('a').eq(i).text())){
-		major_list.push(BASE + catalog('a').eq(i).attr('href'));
+		major_list.push(catalog('a').eq(i).attr('href'));
 	}
 }
 
+// get all the courses and store to classes.json
+for(var i = 0; i < major_list.length; i++){
+	var content = webpageRequest(BASE + major_list[i]);
+	if(content != ''){
+		//if the class is offered
+		course_list[major_list[i].match(/([a-z]+)\.html/)[1].toUpperCase()] = [];
+	}
+}
 
+fs.writeFileSync('classes.json', JSON.stringify(course_list, null, 4));
 
 // pre: give a url that leads to a static webpage
 // post: will return a cheerio HTML dom if the request is successfully

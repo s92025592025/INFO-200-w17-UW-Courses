bd688108a82628d5ac8a86378aa3d49612b003d5
Daniel Wang <s92025592025@gmail.com>
2017-02-14 13:20:45 -0800

    Add starter file and code for module

---
+++ UW-classes.js
@@ -0,0 +1,12 @@
+// @Author: Daniel Wang (JIUN-CHENG WANG)
+// @Description: This module will help to get provided course in UW in Spring 2017
+// 2017/02/14
+
+'use strict';
+
+const fs = require('file-system');
+
+// module reference
+module.exports.testing = function (){
+		console.log('this is the testing');
+;
